f3ab4d0939b78bc01089661ba60d7e6ed789c3be
Vytis Valentinavičius <xytis2000@gmail.com>
2012-02-23 22:48:02 +0200

    Fixed current issues. Working, now go for multithreading =]

--- Logger/Logger.hpp
+++ Logger/Logger.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <stdexcept>
 #include <cstring>
+#include <ctime>
 
 #define log Log::Logger::Instance()->get_stream_with_header(Log::INFO)
 #define err Log::Logger::Instance()->get_stream_with_header(Log::ERROR)
@@ -31,13 +32,172 @@ namespace Log
   };
 
   //Headers
-  std::string empty(PRIORITY priority);
-  std::string date(PRIORITY priority);
-  std::string date_priority(PRIORITY priority); //Default
+  std::string empty(PRIORITY priority)
+  {
+    return "";
+  };
   
-
+  std::string date(PRIORITY priority)
+  {
+    time_t rawtime;
+    time ( &rawtime );
+    char buffer [80];   
+    
+    strftime (buffer,80,"<%Y-%m-%d %H:%M>", localtime ( &rawtime ));
+    return std::string(buffer);
+  };
+  
+  //Default
+  std::string date_priority(PRIORITY priority)
+  {
+    std::string post;
+    switch (priority)
+    {
+      case ERROR:
+        post = " Error: ";
+        break;
+      case WARNING:
+        post = " Warning: ";
+        break;
+      case INFO:
+        post = " Info: ";
+        break;
+      case DEBUG:
+        post = " Debug: ";
+        break;
+      default:
+        throw std::invalid_argument("unknown PRIORITY passed to date_priority");
+    }
+    return date(priority) + post;
+  };
+  
+  /**
+   * class Logger
+   * 
+   * Singleton class, which controls streams to various log channels.
+   */ 
   class Logger
   {
+  public:
+    /**
+     * Singleton instance getter
+     * @return Logger instance
+     */
+    static Logger * Instance()
+    {
+      //Not multithreading safe.
+      return m_instance ? m_instance : m_instance = new Logger();
+    };
+    
+    /**
+     * Singleton cleanup method
+     * Should be used at end of program.
+     */
+    static void destroy()
+    {
+      delete m_instance;
+      m_instance = NULL;
+    };
+    
+    /**
+     * Method returning a stream for given priority
+     * @return std::ostream & output stream.
+     */
+    std::ostream & get_stream(PRIORITY priority)
+    {
+      switch (priority)
+      {
+        case ERROR:
+          return *err_stream;
+        case WARNING:
+          return *warn_stream;
+        case INFO:
+          return *log_stream;
+        case DEBUG:
+          return *debug_stream;
+        default:
+          throw std::invalid_argument("unknown PRIORITY passed to get_stream");
+      }
+    };
+    
+    /**
+     * Method returning a formated stream for given priority
+     * @return std::ostream & formated output stream.
+     */
+    std::ostream & get_stream_with_header(PRIORITY priority)
+    {
+      return get_stream(priority) << (*header)(priority);
+    };
+    
+    /**
+     * Method for linking streams with priorities
+     * @param PRIORITY target priority
+     * @param std::ostream* target stream
+     */
+    void set_output(PRIORITY priority, std::ostream * output)
+    {
+      if (!output)
+        throw std::runtime_error("invalid output stream: 0x0");
+      switch (priority)
+      {
+        case ERROR:
+          log_stream = output;
+          break;
+        case WARNING:
+          warn_stream = output;
+          break;
+        case INFO:
+          log_stream = output;
+          break;
+        case DEBUG:
+          debug_stream = output;
+          break;
+        default:
+          throw std::invalid_argument("unknown PRIORITY passed to set_output");
+      }
+    };
+    
+    /**
+     * Method for setting the header function
+     * @param std::string (*header_generator) (PRIORITY)
+     */
+    void set_header(std::string (*header_generator) (PRIORITY))
+    {
+      if (!header_generator)
+        throw std::runtime_error("invalid header generator function given: 0x0");
+      header = header_generator;
+    };
+    
+    /**
+     * End of recursive variadic template
+     */
+    void formated_output(std::ostream & output, const char *s)
+    {
+      while (*s) {
+        if (*s == '%' && *(++s) != '%')
+          throw std::runtime_error("invalid format string: missing arguments");
+        output << *s++;
+      }
+    };
+    
+    /**
+     * Variadic template, for printing out like fprintf.
+     */
+    template<typename T, typename... Args>
+    void formated_output(std::ostream & output, const char *s, T value, Args... args)
+    {
+      while (*s) {
+        if (*s == '%' && *(++s) != '%') {
+          output << value;
+          ++s;
+          formated_output(output, s, args...); // call even when *s == 0 to detect extra arguments
+        return;
+        }
+        output << *s++;
+      }
+      throw std::logic_error("extra arguments provided to formated_output");
+    };
+    
   private:
     std::ostream * log_stream;
     std::ostream * err_stream;
@@ -49,27 +209,45 @@ namespace Log
     bool header_flag;
 
     //Singleton part
-	Logger();
-    ~Logger();
+    /**
+     * Constructor
+     * 
+     * Sets all channels to std::cout,
+     * default header: date_priority
+     */
+    Logger():
+      log_stream(&std::cout),
+      err_stream(&std::cout),
+      warn_stream(&std::cout),
+      debug_stream(&std::cout),
+      header(date_priority),
+      header_flag(true)
+    {
+      
+    };
+    
+    /**
+     * Destructor
+     * 
+     * Can be called only from inner method.
+     */
+    ~Logger()
+    {
+      
+    };
+    
     Logger(Logger &); //Non existant
     Logger& operator=(Logger const&); //Non existant
     
+    
     static Logger * m_instance;
-  public:
-    static Logger * Instance();
-    static void destroy();
-    
-    std::ostream & get_stream(PRIORITY priority);
-
-    std::ostream & get_stream_with_header(PRIORITY priority);
-
-    void set_output(PRIORITY priority, std::ostream * output);
-    void set_header(std::string (*header_generator) (PRIORITY));
-    
-    void formated_output(std::ostream &, const char *s);
-    template<typename T, typename... Args>
-    void formated_output(std::ostream &,const char *s, T value, Args... args);
+  
   };
+  
+  /**
+   * Singleton init line
+   */
+  Logger* Logger::m_instance = NULL;
 }
 
 #endif
--- Logger/showcase.cpp
+++ Logger/showcase.cpp
@@ -2,33 +2,6 @@
 #include <fstream>
 #include "Logger.hpp"
 
-#define my_print(format, ...) formated_output(std::cout, format , ##__VA_ARGS__)
-#define my_debug(format, ...) formated_output(std::cout, (std::string("[%s:%d] ")+ std::string(format)).c_str(), __FILE__, __LINE__ , ##__VA_ARGS__)
-
-void formated_output(std::ostream & output, const char *s)
-{
-  while (*s) {
-    if (*s == '%' && *(++s) != '%')
-      throw std::runtime_error("invalid format string: missing arguments");
-    output << *s++;
-  }
-}
-
-template<typename T, typename... Args>
-void formated_output(std::ostream & output, const char *s, T value, Args... args)
-{
-  while (*s) {
-    if (*s == '%' && *(++s) != '%') {
-      output << value;
-      ++s;
-      formated_output(output, s, args...); // call even when *s == 0 to detect extra arguments
-        return;
-    }
-    output << *s++;
-  }
-  throw std::logic_error("extra arguments provided to formated_output");
-}
-
 std::string header (Log::PRIORITY)
 {
   return "THIS IS SPARTA: ";
@@ -36,24 +9,15 @@ std::string header (Log::PRIORITY)
 
 int main()
 {
-
-  //Works
   log << "Some random info" << 4 << std::endl;
   err << "Some error" << std::endl;
   warn << "Some warning" << std::endl;
   debug << "debug text" << std::endl;
-
-  my_print("Simple String\n");
-  my_print("%d %d %d\n", 2112, 12, 21);
-  //my_debug("THE WORLD WILL END IN %f SEC\n", 5);
-  //my_debug("debug text\n");
-  
   
   logf("Simple String");
   errf("%d %d %d\n", 2112, 12, 21);
-  //warnf("THE WORLD WILL END IN %f SEC\n", 5);
-  //debugf("debug text\n");
-/*
+  warnf("THE WORLD WILL END IN %f SEC\n", 5);
+  debugf("debug text\n");
 
   std::ofstream file("temp");
   
@@ -61,14 +25,14 @@ int main()
   Log::Logger::Instance()->set_header(header);
 
   log << "Some random info" << 4 << std::endl;
-  err << "Some error";
-  warn << "Some warning";
-  debug << "debug text";
+  err << "Some error" << std::endl;
+  warn << "Some warning" << std::endl;
+  debug << "debug text" << std::endl;
   
   logf("Simple String");
   errf("%d %d %d\n", 2112, 12, 21);
   warnf("THE WORLD WILL END IN %f SEC\n", 5);
   debugf("debug text\n");
-*/
+
   return 0;
 }
